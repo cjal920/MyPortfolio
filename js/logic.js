78fae462d842a98ab4956251779ea97dfd580d9a
Colin Larkin <colinlarkin@Colins-MacBook-Air.local>
2018-06-14 18:20:21 -0400

    updating portfolio with crystal

---
+++ js/logic.js
@@ -0,0 +1,18 @@
+document.getElementById("crystal").onclick = function () {
+    location.href = "index-crystal.html";
+};
+
+/*
+document.getElementById("portfolio").onclick = function () {
+    location.href = "portfolio.html";
+};
+
+document.getElementById("contact").onclick = function () {
+    location.href = "contact.html";
+};
+
+document.getElementById("submit").onclick = function () {
+    location.href = "index.html";
+};
+
+*/
